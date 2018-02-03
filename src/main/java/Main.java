62bf49e3b2e3676da0fbb231b6a871c99999f9c4
Misiekofski <dobrzycki@gmail.com>
2018-02-03 18:31:53 +0100

    Ok, streamy działają :)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,24 +14,31 @@ class Main {
 
 
     public static void main(String[] args) {
-        Deck deck = Deck.newDeck();
-        deck.shuffle();
+//        Deck deck = Deck.newDeck();
+//        deck.shuffle();
+//
+//        List<Card> hand1 = deal(deck, 5);
+//        List<Card> hand2 = deal(deck, 5);
+//        List<Card> hand3 = deal(deck, 5);
+//        List<Card> rest = deal(deck, 37);
+//
+//        System.out.println("Ręka 1 gracza:");
+//        hand1.forEach(card -> System.out.println(card.getLabel()));
+//        System.out.println();
+//
+//        System.out.println("Ręka 2 gracza:");
+//        hand2.forEach(card -> System.out.println(card.getLabel()));
+//        System.out.println();
+//
+//        System.out.println("Ręka 3 gracza:");
+//        hand3.forEach(card -> System.out.println(card.getLabel()));
+//        System.out.println();
+//
+//        System.out.println("Pozostałe karty:");
+//        rest.forEach(card -> System.out.println(card.getLabel()));
+        Deck anotherdeck = Deck.newDeck();
+        List<Card> wszystkiekarty = deal(anotherdeck, 52);
+        wszystkiekarty.forEach(card -> System.out.println(card.getLabel()));
 
-        List<Card> hand1 = deal(deck, 5);
-        List<Card> hand2 = deal(deck, 5);
-        List<Card> hand3 = deal(deck, 5);
-
-
-        System.out.println("Player one hand:");
-        hand1.forEach(card -> System.out.println(card.getLabel()));
-        System.out.println();
-
-        System.out.println("Player two hand:");
-        hand2.forEach(card -> System.out.println(card.getLabel()));
-        System.out.println();
-
-        System.out.println("Player three hand:");
-        hand2.forEach(card -> System.out.println(card.getLabel()));
-        System.out.println();
     }
 }
